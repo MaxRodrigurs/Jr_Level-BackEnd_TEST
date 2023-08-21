7e833af2c5f2343d2d64905756f18a8c7667d3b6
MaxRodrigurs <103291417+MaxRodrigurs@users.noreply.github.com>
2023-08-21 16:03:47 -0300

    feat: initial configuration.

---
+++ src/test/java/com/PicPayBackend_Challenge/PicPayBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicPayBackend_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
