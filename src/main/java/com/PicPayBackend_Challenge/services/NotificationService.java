24b928470c172c7985fb0e843dd27cc74bcdc8b9
MaxRodrigurs <103291417+MaxRodrigurs@users.noreply.github.com>
2023-08-26 14:53:39 -0300

    feat: Create a Notification Service.

---
+++ src/main/java/com/PicPayBackend_Challenge/services/NotificationService.java
@@ -0,0 +1,28 @@
+package com.PicPayBackend_Challenge.services;
+
+import com.PicPayBackend_Challenge.domain.user.User;
+import com.PicPayBackend_Challenge.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception{
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Erro de notificação");
+            throw new Exception("Serviço Indisponivel no momento.");
+        }
+    }
+}
