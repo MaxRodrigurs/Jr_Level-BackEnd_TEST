a56ecd04621dab0ce189ffedc09a43502340044e
MaxRodrigurs <103291417+MaxRodrigurs@users.noreply.github.com>
2023-08-26 14:55:51 -0300

    feat: Create a Exception Controller to have a better hand of the Exceptions.

---
+++ src/main/java/com/PicPayBackend_Challenge/Infra/ExceptionController.java
@@ -0,0 +1,28 @@
+package com.PicPayBackend_Challenge.Infra;
+
+import com.PicPayBackend_Challenge.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já se encontra no banco de dados.", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threatDuplicateEntry(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
