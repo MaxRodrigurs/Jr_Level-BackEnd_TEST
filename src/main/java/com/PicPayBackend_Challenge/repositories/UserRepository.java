92655dc4fba9dea9aaf6009e0cc1ae5a3c5e6cc4
MaxRodrigurs <103291417+MaxRodrigurs@users.noreply.github.com>
2023-08-26 15:08:09 -0300

    Removing Comments.

--- src/main/java/com/PicPayBackend_Challenge/repositories/UserRepository.java
+++ src/main/java/com/PicPayBackend_Challenge/repositories/UserRepository.java
@@ -8,7 +8,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long Id);
-    //Método definido para achar o usuario pelo documento, contudo
-    //ele pode ter um retorno ou não, por isso o Optional.
-    //Eu apenas declarei sem implementar pois o JPA consegue montar a query em runtime apenas com o que eu já defini.
 }
